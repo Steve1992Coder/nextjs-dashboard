2d0050ebe66f5175e70da5d964273bab6798bbb5
Steve1992Coder <1561309347@qq.com>
2023-11-17 16:46:39 +0800

    测试 fetch sql data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+const bcrypt = require('bcrypt');
+export default async function Page() {
+  const hashedPassword = await bcrypt.hash('123456', 10);
+  return <p>Customer Page ${hashedPassword}</p>;
 }
